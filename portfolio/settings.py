0ff51ef0f192c9d65329f6f4bae50cfb0541aa95
Kaladinn <danielskulimowski.biuro@gmail.com>
2023-06-14 11:40:41 +0200

    x

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,7 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/mainsite/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'mainsite' / 'static',
+]
+
+STATIC_URL = '/static/'
+
 STATIC_ROOT = '/mainsite/static/'
 
 
